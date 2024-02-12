b208b6f47b0232695f5cf293d6b485f28ded47fa
NicolasShaneLynch <148854080+NicolasShaneLynch@users.noreply.github.com>
2024-02-12 13:43:17 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 /** Costante di environment per la produzione */
 export const environment = {
   production: false,
-  apiUrl: "http://172.30.119.75:8080/"
+  apiUrl: "http://172.30.101.239:8080"
 };
