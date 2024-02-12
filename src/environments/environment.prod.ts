232fa30be32ac02edb05baec9c2740897aab6b14
NicolasShaneLynch <148854080+NicolasShaneLynch@users.noreply.github.com>
2024-02-12 14:09:27 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 /** Costante di environment per la produzione */
 export const environment = {
   production: true,
- apiUrl: "http://${REMOTESERVER}:${PORTAGATEWAY}"
+  apiUrl: "http://172.30.101.239:8080"
 };
